2ddf535bb0cfa5d848d46503639a6b2c1ed9e366
Bhoomika R <72621958+Bhoomika-R@users.noreply.github.com>
2022-08-26 10:07:34 +0530

    Update cms.sql

--- DBMS-Mini-Project/cms.sql
+++ DBMS-Mini-Project/cms.sql
@@ -1,19 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 3.2.0.1
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Oct 23, 2013 at 12:50 PM
--- Server version: 5.5.8
--- PHP Version: 5.3.3
-
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `cms`
